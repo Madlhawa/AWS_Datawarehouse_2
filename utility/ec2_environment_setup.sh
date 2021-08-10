98346d8ed6982cc3daf2a533e25d5ba796a7511f
madlhawa <madhawa242@gmail.com>
2021-08-10 09:08:27 +0530

    q

--- utility/ec2_environment_setup.sh
+++ utility/ec2_environment_setup.sh
@@ -1,5 +1,8 @@
 sudo apt update
+sudo apt install libpq-dev python3-dev
 sudo apt install git
 sudo apt install python3-pip
 pip3 install boto3
 pip3 install pandas
+pip3 install sqlalchemy
+pip3 install psycopg2
