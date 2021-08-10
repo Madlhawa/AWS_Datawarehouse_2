85910b91363ca85a7d74f0e43f6e1404df4c2844
madlhawa <madhawa242@gmail.com>
2021-08-10 09:16:24 +0530

    q

--- python_scripts/unload-redshift.py
+++ python_scripts/unload-redshift.py
@@ -1,10 +1,15 @@
+import sys
 from sqlalchemy import create_engine
 from configparser import ConfigParser
 
+try:
+        table = sys.argv[1]
+except:
+        print("ERROR: Table name argument not passed.")
+        quit()
+
 config = ConfigParser()
 config.read('./utility/config.ini')
-
-table = 'order'
 
 engine = create_engine(f'postgresql://{config["redshift"]["username"]}:{config["redshift"]["password"]}@redshift-cluster.cy8zz3a1tpnj.us-east-1.redshift.amazonaws.com:5439/dev')
 
